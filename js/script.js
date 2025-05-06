c1821ee1fd3d189a8cd7820f03a5d2a52296767c
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-05-06 00:51:49 +0000

    template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia Ten Den
+// Created on: April 28, 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function checkDiscount() {
+  let age = parseInt(document.getElementById("age").value);
+  let dayOfTheWeek = document.getElementById("day");
+  let value = dayOfTheWeek.options[dayOfTheWeek.selectedIndex].value;
+
+  if (value == "tuesday" || value == "thursday" || (age > 12 && age < 21)) {
+    document.getElementById('result').innerHTML = "<p>You are eligible for the discount.</p>";
+  } else {
+    document.getElementById('result').innerHTML = "<p>You are NOT eligible for the discount.</p>";
+  }
+}
